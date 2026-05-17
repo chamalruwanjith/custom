aa423a3d4db47ee8ed848beb2acb6af8ba93fc15
chamalr-nisus <chamal@nisus.lk>
2026-05-17 19:38:17 +0530

    [FIX] vkd_property_management : - "UnitReservation created" chatter entry shows the real agent

--- vkd_property_management/__manifest__.py
+++ vkd_property_management/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': "Property Management",
-    'version': '17.0.1.3.5',
+    'version': '17.0.1.3.6',
     'category': 'Sales',
 
     'summary': """This module offers dynamic property configuration, unit reservations, real-time stock checks,
